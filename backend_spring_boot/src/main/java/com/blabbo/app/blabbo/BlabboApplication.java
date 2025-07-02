bc324b6564216a43f2b6d7224f17032fbebae5da
Andreas Jansson <72551981+P90k@users.noreply.github.com>
2025-07-02 18:14:16 +0200

    setup springboot backend

---
+++ backend_spring_boot/src/main/java/com/blabbo/app/blabbo/BlabboApplication.java
@@ -0,0 +1,13 @@
+package com.blabbo.app.blabbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlabboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlabboApplication.class, args);
+	}
+
+}
---
+++ backend_spring_boot/src/test/java/com/blabbo/app/blabbo/BlabboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blabbo.app.blabbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlabboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
