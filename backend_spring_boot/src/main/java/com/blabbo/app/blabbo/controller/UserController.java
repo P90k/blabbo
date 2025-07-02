ce67fd0364279bdce5f8d34d764aec095316a338
Andreas Jansson <andreas.jansson234@gmail.com>
2025-07-02 21:39:13 +0200

    create table users, build CRUD methods

---
+++ backend_spring_boot/src/main/java/com/blabbo/app/blabbo/controller/UserController.java
@@ -0,0 +1,86 @@
+package com.blabbo.app.blabbo.controller;
+import com.blabbo.app.blabbo.model.User;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.blabbo.app.blabbo.repository.UserRepository;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+            return userRepository.findById(id)
+            .map(user -> ResponseEntity.ok(user))
+            .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        String userName = userDetails.getName();
+        String email = userDetails.getEmail();
+
+        if (userName != null) {
+            user.setName(userName);
+        }
+        if (email != null) {
+            user.setEmail(email);
+        }
+
+        User savedUser = userRepository.save(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
+
+
+    @PostMapping("/create")
+    public ResponseEntity<User> createUser(@RequestBody User newUser) {
+
+        // Check for existing user-email in db
+        Optional<User> existingUser = userRepository.findByEmail(newUser.getEmail());
+
+        if (existingUser.isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
+        User savedUser = userRepository.save(newUser);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
+
+
+     @DeleteMapping("/{id}")
+     public void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+     }
+
+}
---
+++ backend_spring_boot/src/main/java/com/blabbo/app/blabbo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.blabbo.app.blabbo.repository;
+import com.blabbo.app.blabbo.model.User;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
