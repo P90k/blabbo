23987e888ab8748417e95ef2a25245ed6ceabfe8
Andreas Jansson <andreas.jansson234@gmail.com>
2025-07-05 05:15:34 +0200

    Add password, sentMsgs, and receivedMsgs fields to User entity
    Adds fields for password storage and tracking of sent and received messages. Includes corresponding getters, setters, and toString implementation.

--- backend_spring_boot/src/main/java/com/blabbo/app/blabbo/model/User.java
+++ backend_spring_boot/src/main/java/com/blabbo/app/blabbo/model/User.java
@@ -1,50 +1,100 @@
 package com.blabbo.app.blabbo.model;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "users")
 public class User {
     @Id // primary key
-    @GeneratedValue(strategy =  GenerationType.IDENTITY) // autoincrement
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // autoincrement
     private Long id;
+
+    @NotBlank
+    @Size(min = 4, max = 15)
     private String name;
+
+    @Email
+    @NotBlank
     private String email;
-    
 
-    public User() {}
+    @NotBlank
+    @Size(min = 8, max = 20)
+    private String password;
 
-    public User(String name, String email) {
-        this.name = name;
-        this.email = email;
+    @OneToMany(mappedBy = "sender")
+    private List<Message> sentMessages;
+
+    @OneToMany(mappedBy = "recipient")
+    private List<Message> recievedMessages;
+
+    public User() {
     }
 
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 
     public Long getId() {
         return id;
     }
 
-
     public String getName() {
         return name;
     }
-
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String getEmail() {
         return email;
     }
 
-
     public void setEmail(String email) {
         this.email = email;
+
     }
+
+    public List<Message> getSentMessages() {
+        return sentMessages;
+    }
+
+    public void setSentMessages(List<Message> sentMessages) {
+        this.sentMessages = sentMessages;
+    }
+
+    public List<Message> getRecievedMessages() {
+        return recievedMessages;
+    }
+
+    public void setRecievedMessages(List<Message> recievedMessages) {
+        this.recievedMessages = recievedMessages;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", sentMessages="
+                + sentMessages + ", recievedMessages=" + recievedMessages + "]";
+    }
+
 }
