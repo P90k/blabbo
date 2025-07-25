147e76fd2592b8962d0b312c904971fe07ab468a
Andreas Jansson <andreas.jansson234@gmail.com>
2025-07-25 20:20:26 +0200

    Add method existsbyEmail to userRepository

--- backend_spring_boot/src/main/java/com/blabbo/app/blabbo/repository/UserRepository.java
+++ backend_spring_boot/src/main/java/com/blabbo/app/blabbo/repository/UserRepository.java
@@ -9,7 +9,9 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email);
+    User findByEmail(String email);
+
+    Boolean existsByEmail(String email);
 
     List<UserSummary> findAllProjectedBy();
 }
