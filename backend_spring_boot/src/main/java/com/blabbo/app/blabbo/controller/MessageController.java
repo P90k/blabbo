0f365054bed112d2a91845445ff4adb94d412b20
Andreas Jansson <andreas.jansson234@gmail.com>
2025-07-25 21:11:34 +0200

    code cleanup, remove unused field in MessageController

--- backend_spring_boot/src/main/java/com/blabbo/app/blabbo/controller/MessageController.java
+++ backend_spring_boot/src/main/java/com/blabbo/app/blabbo/controller/MessageController.java
@@ -3,7 +3,6 @@ package com.blabbo.app.blabbo.controller;
 import com.blabbo.app.blabbo.DTO.MessageDTO;
 import com.blabbo.app.blabbo.model.Message;
 import com.blabbo.app.blabbo.service.interfaces.MessageService;
-import com.blabbo.app.blabbo.service.MessageServiceImp;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,13 +14,10 @@ import java.util.List;
 @RequestMapping("/api/message")
 public class MessageController {
 
-    private final MessageService msgService;
     private final MessageService messageService;
 
 
-    public MessageController(MessageServiceImp msgService,
-                             MessageService messageService) {
-        this.msgService     = msgService;
+    public MessageController(MessageService messageService) {
         this.messageService = messageService;
     }
 
@@ -30,29 +26,31 @@ public class MessageController {
     public ResponseEntity<Message> postMethodName(
             @RequestBody MessageDTO messageDTO) {
 
-        Message newMessage = msgService.createMessage(messageDTO.getContent(),
-                                                      messageDTO.getSenderEmail(),
-                                                      messageDTO.getRecipientEmail());
-        msgService.sendMessage(newMessage);
+        Message newMessage =
+                messageService.createMessage(messageDTO.getContent(),
+                                             messageDTO.getSenderEmail(),
+                                             messageDTO.getRecipientEmail());
+        messageService.sendMessage(newMessage);
         return ResponseEntity.ok().body(newMessage);
     }
 
 
     @GetMapping("/{id}")
     public ResponseEntity<MessageDTO> getMessage(@PathVariable Long id) {
-        MessageDTO retrievedMsg = msgService.getMessage(id);
+        MessageDTO retrievedMsg = messageService.getMessage(id);
         return ResponseEntity.ok(retrievedMsg);
     }
 
+
     @GetMapping("")
     public ResponseEntity<List<MessageDTO>> getAllMessages() {
-        return ResponseEntity.ok(msgService.getMessages());
+        return ResponseEntity.ok(messageService.getMessages());
     }
 
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteMessage(@PathVariable Long id) {
-        msgService.deleteMessage(id);
+        messageService.deleteMessage(id);
         // 204 indicates successful deletion
         return ResponseEntity.status(HttpStatus.valueOf(204)).build();
     }
